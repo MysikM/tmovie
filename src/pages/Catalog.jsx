12796166e8a134ef1526065b6ee581869b0da417
Mysik <Mishamysik@gmail.com>
2022-04-18 20:31:38 +0300

    catalog page add

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -5,6 +5,7 @@ import {useParams} from "react-router-dom";
 import PageHeader from "../component/page-header/PageHeader";
 
 import {category as cate} from "../api/tmdbApi";
+import MovieGrid from "../component/movie-grid/MovieGrid";
 
 const Catalog = () => {
 
@@ -15,6 +16,11 @@ const Catalog = () => {
             <PageHeader>
                 {category === cate.movie ? 'Movies' : 'TV Series'}
             </PageHeader>
+            <div className="container">
+                <div className="section mb-3">
+                    <MovieGrid category={category} />
+                </div>
+            </div>
         </>
     );
 };
