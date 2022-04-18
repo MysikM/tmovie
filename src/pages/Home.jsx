88f4ca35306d1a151bb3fcda58c77de8486c2d19
Mysik <Mishamysik@gmail.com>
2022-04-18 18:15:55 +0300

    home page add link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
 import HeroSlide from "../component/hero-slide/HeroSlide";
+import {Link} from "react-router-dom";
+import {OutlineButton} from "../component/button/Button";
 
 const Home = () => {
     return (
         <>
             <HeroSlide />
+            <div className="container">
+                <div className="section mb-3">
+                    <div className="section__header mb-2">
+                        <h2>Trending Movies</h2>
+                        <Link to='/movie'>
+                            <OutlineButton className="name">
+                                View more
+                            </OutlineButton>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
