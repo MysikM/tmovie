08462d7fa9ce155ee3c04a8a6f05583218020983
Mysik <Mishamysik@gmail.com>
2022-04-18 12:30:38 +0300

    routes add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import {BrowserRouter, Route} from 'react-router-dom';
+import 'swiper/swiper.min.css';
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+
 import './App.scss';
+
+import Header from "./component/header/Header";
+import Footer from "./component/footer/Footer";
+import NavPage from  "./config/NavPage";
 
 function App() {
   return (
-    <div>
-      App
-    </div>
+    <BrowserRouter>
+         <Header/>
+         <NavPage />
+         <Footer />
+    </BrowserRouter>
   );
 }
 
